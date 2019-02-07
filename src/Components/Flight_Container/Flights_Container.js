bca9f9be1f6baab2ecd294eef673f641a1148b6e
Mark Romero <markromero@marks-mbp.lan>
2019-02-07 00:57:45 -0700

    updated flights container to a functional component

--- src/Components/Flight_Container/Flights_Container.js
+++ src/Components/Flight_Container/Flights_Container.js
@@ -1,41 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import FlightCard from './Flight_Card/Flight_Card';
 import './Flight_Container.css';
 
-export default class FlightsContainer extends Component {
-    constructor(props) {
-        super(props);
-    }
+export default function FlightsContainer(props) {
 
-    removeFlight = (flightId) => {
-        this.props.remove(flightId);
-    }
-
-    editFlight = (flight, updatedComment) => {
-        this.props.editFlight(flight, updatedComment);
-    }
-
-    render() {
-        const flights = this.props.flights.map( (flight) => {
+    const flights = props.flights.map( (flight) => {
             return (
-                <FlightCard key={flight.flightNumber} flight={flight} editFlight={this.editFlight} remove={ () => this.removeFlight(flight.flightNumber)}/>
+                <FlightCard key={flight.flightNumber} flight={flight} editFlight={ () => props.editFlight(flight)} remove={ () => props.remove(flight.flightNumber)}/>
             );
-          });
-        return (    
-            <section className='flights-container-section'>
-                { this.props.flights.length === 0 ? (
-                    <div className="flight-container-title">Add some Flights</div>
-                ) : (
-                    <div>
-                        <h2 className='flight-container-title'>Flights</h2>
-                        <div className="flights-container">
-                            {flights}
-                        </div> 
-                    </div>
-                    
-                )
-                }
-            </section>
-        )
-  }
+    });
+    return (    
+        <section className='flights-container-section'>
+            { props.flights.length === 0 ? (
+                <div className="flight-container-title">Add some Flights</div>
+            ) : (
+                <div>
+                    <h2 className='flight-container-title'>Flights</h2>
+                    <div className="flights-container">
+                        {flights}
+                    </div> 
+                </div>
+                
+            )
+            }
+        </section>
+    )
 }
